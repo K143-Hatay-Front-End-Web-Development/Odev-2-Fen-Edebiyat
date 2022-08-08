4c668fcefc191cff3909264e9e6c7f75627aa871
Aysen Aydin <aysen904@gmail.com>
2022-08-09 01:51:11 +0300

    tasarim duzenlemesi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 
 function App() {
   return (
     <div className="App">
-      <div className='title'>Game Time</div>
-      <div className='Board'>
-    
-        <div className='game--container'>
-         <div data-cell-index="0" class='cell'></div>
-         <div data-cell-index="1" class='cell'></div>
-         <div data-cell-index="2" class='cell'></div>
-         <div data-cell-index="3" class='cell'></div>
-         <div data-cell-index="4" class='cell'></div>
-         <div data-cell-index="5" class='cell'></div>
-         <div data-cell-index="6" class='cell'></div>
-         <div data-cell-index="7" class='cell'></div>
-         <div data-cell-index="8" class='cell'></div>
-        </div>
+      <div className='title'>GAME TIME</div>
+      <div className='game--container'>
+        <div data-cell-index="0" class='cell'></div>
+        <div data-cell-index="1" class='cell'></div>
+        <div data-cell-index="2" class='cell'></div>
+        <div data-cell-index="3" class='cell'></div>
+        <div data-cell-index="4" class='cell'></div>
+        <div data-cell-index="5" class='cell'></div>
+        <div data-cell-index="6" class='cell'></div>
+        <div data-cell-index="7" class='cell'></div>
+        <div data-cell-index="8" class='cell'></div>
+
       </div>
+
     </div>
   );
 }
